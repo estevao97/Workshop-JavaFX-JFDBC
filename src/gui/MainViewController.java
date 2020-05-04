e69e285eda1ae8d360197e6c6fe2db27a5c408ca
Paulo Estêvão <pauloestevao92@gmail.com>
2020-05-04 01:23:48 -0300

    DepartmentList view disign

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,73 +1,49 @@
 package gui;
 
 import java.io.IOException;
-
 import java.net.URL;
-
 import java.util.ResourceBundle;
-
 import application.Main;
-
 import gui.util.Alerts;
-
 import javafx.fxml.FXML;
-
 import javafx.fxml.FXMLLoader;
-
 import javafx.fxml.Initializable;
-
 import javafx.scene.Node;
-
 import javafx.scene.Scene;
-
 import javafx.scene.control.Alert.AlertType;
-
 import javafx.scene.control.MenuItem;
-
 import javafx.scene.control.ScrollPane;
-
 import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
 	@FXML
-
 	private MenuItem menuItemSeller;
 
 	@FXML
-
 	private MenuItem menuItemDepartment;
 
 	@FXML
-
 	private MenuItem menuItemAbout;
 
 	@FXML
-
 	public void onMenuItemSellerAction() {
 
 		System.out.println("onMenuItemSellerAction");
-
 	}
 
 	@FXML
-
 	public void onMenuItemDepartmentAction() {
-
-		System.out.println("onMenuItemDepartmentAction");
-
+		loadView("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
-
 	public void onMenuItemAboutAction() {
 
 		loadView("/gui/About.fxml");
-
 	}
 
 	@Override
-
 	public void initialize(URL uri, ResourceBundle rb) {
 
 	}
